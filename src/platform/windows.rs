dc32d739195506782f4cb2f8dc635ea4371bdb21
Vildan <vildankusamatov3978@gmail.com>
2024-04-13 15:35:39 +0500

    Fix windows implementation

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,4 +1,5 @@
 use crate::{Clipboard, ClipboardBinaryKind, ClipboardContent, ClipboardTextKind, Result};
+use clipboard_win::image::Image;
 pub use clipboard_win::Clipboard as WindowsClipboard;
 
 pub fn clipboard() -> Result<impl Clipboard> {
@@ -9,19 +10,16 @@ impl Clipboard for WindowsClipboard {
     fn set_content(&mut self, content: ClipboardContent) -> Result<()> {
         match content {
             ClipboardContent::Text { text, kind } => match kind {
-                ClipboardTextKind::Unicode => {
-                    let bytes: &[u16] = &text.encode_utf16().collect();
-
-                    // UTF-16 text format
-                    self.set_data(13, &bytes)
-                }
+                ClipboardTextKind::UTF8 => self.set_string(&text).map_err(Into::into),
             },
             ClipboardContent::Binary { data, kind } => match kind {
                 ClipboardBinaryKind::Image => {
-                    let data: &[u8] = data.into();
+                    let image = Image {
+                        // TODO: Optimize this or use another crate
+                        bytes: data.to_vec(),
+                    };
 
-                    // Bitmap format
-                    self.set_data(2, &data)
+                    self.set_bitmap(&image).map_err(Into::into)
                 }
             },
         }
@@ -32,6 +30,8 @@ impl Clipboard for WindowsClipboard {
 
         self.get_string(&mut string)?;
 
-        Ok(string)
+        let content = ClipboardContent::from_plain_string(string);
+
+        Ok(content)
     }
 }
