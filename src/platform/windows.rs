57050cfd5472f347d8264c536b0ec85a1bcdb56c
Vildan <vildankusamatov3978@gmail.com>
2024-04-13 13:40:36 +0500

    Implement windows clipboard

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,17 +1,37 @@
-use crate::{Clipboard, NotImplementedError, Result};
+use crate::{Clipboard, ClipboardBinaryKind, ClipboardContent, ClipboardTextKind, Result};
+pub use clipboard_win::Clipboard as WindowsClipboard;
 
 pub fn clipboard() -> Result<impl Clipboard> {
-    Ok(WindowsClipboard)
+    WindowsClipboard::new().map_err(Into::into)
 }
 
-pub struct WindowsClipboard;
-
 impl Clipboard for WindowsClipboard {
-    fn get_content(&self) -> Result<crate::ClipboardContent> {
-        Err(Box::new(NotImplementedError))
+    fn set_content(&mut self, content: ClipboardContent) -> Result<()> {
+        match content {
+            ClipboardContent::Text { text, kind } => match kind {
+                ClipboardTextKind::Unicode => {
+                    let bytes: &[u16] = &text.encode_utf16().collect();
+
+                    // UTF-16 text format
+                    self.set_data(13, &bytes)
+                }
+            },
+            ClipboardContent::Binary { data, kind } => match kind {
+                ClipboardBinaryKind::Image => {
+                    let data: &[u8] = data.into();
+
+                    // Bitmap format
+                    self.set_data(2, &data)
+                }
+            },
+        }
     }
 
-    fn set_content(&mut self, _content: crate::ClipboardContent) -> Result<()> {
-        Err(Box::new(NotImplementedError))
+    fn get_content(&self) -> Result<ClipboardContent> {
+        let mut string = String::new();
+
+        self.get_string(&mut string)?;
+
+        Ok(string)
     }
 }
